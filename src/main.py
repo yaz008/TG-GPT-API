745d9b276bc1957afb32a7aa9f3497ea1ce19c99
Emelianov Artem <yaz008.yaz008@yandex.ru>
2024-09-11 15:45:05 +0300

    Add user filter

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from app import app
 from pyrogram.client import Client
+from pyrogram.filters import user
 from pyrogram.types import Message
 from threading import Thread
 from server import server
@@ -9,7 +10,7 @@ GPT: str = '@GPT_chat_robot'
 def callback(message: str) -> str:
     app.send_message(chat_id=GPT, text=message)
 
-@app.on_message(filters=None)
+@app.on_message(filters=user(GPT))
 def on_message(_: Client, message: Message) -> None:
     server.send(message=message.text)
 
