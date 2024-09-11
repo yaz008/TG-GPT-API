e97050e7b1a14ac1103c2c1dbafb6849bf843610
Emelianov Artem <yaz008.yaz008@yandex.ru>
2024-09-11 15:41:19 +0300

    Add GPT API to main

--- src/main.py
+++ src/main.py
@@ -1,10 +1,20 @@
 from app import app
 from pyrogram.client import Client
 from pyrogram.types import Message
+from threading import Thread
+from server import server
+
+GPT: str = '@GPT_chat_robot'
+
+def callback(message: str) -> str:
+    app.send_message(chat_id=GPT, text=message)
+    return 'Generating response...'
 
 @app.on_message(filters=None)
 def on_message(_: Client, message: Message) -> None:
-    print(message.text)
+    server.send(message=message.text)
 
 if __name__ == '__main__':
+    server.callback = callback
+    Thread(target=server.accept).start()
     app.run()
