41ccf5561abaf6b086f6e8dcc567e8a162aee5b2
Emelianov Artem <yaz008.yaz008@yandex.ru>
2024-10-09 01:22:26 +0300

    Change app.name to TG-GPT-API

--- src/app/instance.py
+++ src/app/instance.py
@@ -5,7 +5,7 @@ from pyrogram import Client
 
 if not load_dotenv():
     raise Exception('Unable to load .env file')
-app: Client = Client(name=__name__,
+app: Client = Client(name='TG-GPT-API',
                      api_id=getenv(key='API_KEY'),
                      api_hash=getenv(key='API_HASH'),
                      workdir=f'{path[0]}\\app\\__session__')
