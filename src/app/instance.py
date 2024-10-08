dfd84a6df4041007d75d525037d8efa0891175bc
Emelianov Artem <yaz008.yaz008@yandex.ru>
2024-10-09 01:21:36 +0300

    Set app.workdir to src/app/__session__

--- src/app/instance.py
+++ src/app/instance.py
@@ -1,9 +1,11 @@
 from os import getenv
 from dotenv import load_dotenv
+from sys import path
 from pyrogram import Client
 
 if not load_dotenv():
     raise Exception('Unable to load .env file')
 app: Client = Client(name=__name__,
                      api_id=getenv(key='API_KEY'),
-                     api_hash=getenv(key='API_HASH'))
+                     api_hash=getenv(key='API_HASH'),
+                     workdir=f'{path[0]}\\app\\__session__')
